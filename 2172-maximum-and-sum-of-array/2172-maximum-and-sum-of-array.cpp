2ee4f5317a83f8d02669d3ee6e4cd6974ea28c0d
Kirthik-Roshan <kirthikroshan60@gmail.com>
2025-11-14 20:24:34 +0530

    Time: 27 ms (83.76%), Space: 12.9 MB (86.29%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int maximumANDSum(vector<int>& nums, int numSlots) {
-        int n = nums.size();
-        int m = numSlots * 2;                // total positions
-        int M = 1 << m;
-        vector<int> dp(M, 0);
+     int n = (int)nums.size();
+    vector<int> pow3(numSlots + 1, 1);
+    for (int i = 1; i <= numSlots; ++i) pow3[i] = pow3[i - 1] * 3;
 
-        for (int mask = 0; mask < M; ++mask) {
-            int used = __builtin_popcount((unsigned)mask);
-            if (used >= n) continue;        // already placed all numbers or too many
-            int curNum = nums[used];        // place this number next
-            for (int pos = 0; pos < m; ++pos) {
-                if ((mask >> pos) & 1) continue;    // pos already used
-                int newMask = mask | (1 << pos);
-                int slot = pos / 2 + 1;
-                dp[newMask] = max(dp[newMask], dp[mask] + (curNum & slot));
+    int totalStates = pow3[numSlots];
+    vector<int> memo(totalStates, -1);
+
+    function<int(int,int)> dfs = [&](int mask, int idx) -> int {
+        if (idx == n) return 0;
+        int& res = memo[mask];
+        if (res != -1) return res;
+        int best = 0;
+        for (int s = 1; s <= numSlots; ++s) {
+            int occ = (mask / pow3[s - 1]) % 3;
+            if (occ < 2) {
+                int nextMask = mask + pow3[s - 1];
+                best = max(best, (nums[idx] & s) + dfs(nextMask, idx + 1));
             }
         }
+        return res = best;
+    };
 
-        int ans = 0;
-        for (int mask = 0; mask < M; ++mask) {
-            if (__builtin_popcount((unsigned)mask) == n) ans = max(ans, dp[mask]);
-        }
-        return ans;
+    return dfs(0, 0);
     }
 };
