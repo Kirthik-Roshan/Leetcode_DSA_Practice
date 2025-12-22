127413699cd979790810a3baea540c2752b80438
Kirthik-Roshan <kirthikroshan60@gmail.com>
2025-12-22 20:46:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,3 +1,6 @@
-DELETE p1 FROM Person p1
-JOIN Person p2 
-ON p1.email = p2.email AND p1.id > p2.id;
+Select d.name as department , e1.name as employee, e1.salary as Salary
+From Employee e1 join Department d on e1.DepartmentId = d.Id
+Where  3 > (select count(distinct (e2.Salary))
+            from  Employee e2
+            where e2.Salary > e1.Salary
+            and e1.DepartmentId = e2.DepartmentId)
