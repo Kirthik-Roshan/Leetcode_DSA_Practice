b14247e9967cc06708923f44e8eca2025ee4a991
Kirthik-Roshan <kirthikroshan60@gmail.com>
2026-03-29 08:08:24 +0530

    Time: 0 ms (100%), Space: 8.9 MB (28.57%) - LeetHub

---
+++ 101026-first-matching-character-from-both-ends/101026-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        for(int i=0;i<s.size();i++){
+            if(s[i]==s[s.size()-i-1]){
+                return i;
+                break;
+            }
+        }
+        return -1;
+    }
+};
