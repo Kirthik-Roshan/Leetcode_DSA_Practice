fe3621fc16da484d1e154388e47158779232a016
Kirthik-Roshan <kirthikroshan60@gmail.com>
2026-05-15 11:34:35 +0530

    Time: 0 ms (100%), Space: 14.2 MB (48.18%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,22 +1,6 @@
-#pragma GCC optimize ("Ofast")
-#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
-#pragma GCC optimize ("-ffloat-store")
-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-        int left = 0, right = nums.size() - 1;
-
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(nums[mid] < nums[right]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return nums[left];
+        return *min_element(nums.begin(),nums.end());
     }
 };
